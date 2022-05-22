b6ccbf9022b2884ec2a1df5b9c4bb88f7d255b0e
Josh Justice <me@codingitwrong.com>
2022-05-22 08:41:52 -0400

    Configure linting and formatting

---
+++ e2e/firstTest.e2e.js
@@ -0,0 +1,15 @@
+
+        describe('Example', () => {
+          beforeAll(async () => {
+            await device.launchApp();
+          });
+
+          beforeEach(async () => {
+            await device.reloadReactNative();
+          });
+
+          it('should say hello', async () => {
+            await expect(element(by.text('Hello, React Native!'))).toBeVisible();
+          });
+        });
+      
